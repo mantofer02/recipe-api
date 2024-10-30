e7e50afa9f10edd44f32794441aa420f60966951
mantofer02 <mantofer2000@gmail.com>
2024-10-29 23:23:58 -0600

    Add core app and update database settings for environment variable access

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -76,10 +77,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ('DB_HOST'),
-        'NAME': os.environ('DB_NAME'),
-        'USER': os.environ('DB_USER'),
-        'PASSWORD': os.environ('DB_PASS')
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS')
     }
 }
 
